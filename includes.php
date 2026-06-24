976f01a5d0bd5d99efc6f1a660daf59dd3a7c1fe
Damian Zaremba <damian@damianzaremba.co.uk>
2026-06-24 16:55:31 +0200

    Rename IRC to Relay

--- includes.php
+++ includes.php
@@ -39,7 +39,7 @@ require_once 'cluebot-ng.config.php';
 require_once 'action_functions.php';
 require_once 'cbng.php';
 require_once 'irc_feed_functions.php';
-require_once 'irc_functions.php';
+require_once 'relay_functions.php';
 require_once 'mysql_functions.php';
 require_once 'globals.php';
 require_once 'api.php';
